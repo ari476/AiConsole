8a8c3981ad0801e6c83e95ea1f7fba2203a9304f
ari refael <AriRe@edu.aac.ac.il>
2022-03-03 21:50:42 +0200

    comment and efficiency

--- AiGuseeConsole/Program.cs
+++ AiGuseeConsole/Program.cs
@@ -6,24 +6,35 @@ namespace AiGuseeConsole
 {
     class Program
     {
-        static  int[] allCombination = getAllCombination();
+        // allCombination - Array taht include all the numbers in the game
+        static int[] allCombination = getAllCombination();
+        // allGuess - All the guesses that the computer guessed
         static int[] allGuess = new int[10];
+        // responseToGuess - An array of pairs that contains all 
+        //the user's responses according to the computer's guess
         static Tuple<int, int>[] responseToGuess = new Tuple<int, int>[10];
+        
         static void Main(string[] args)
         {
-
-            int i;
+            // i - The current index of number to check if 
+            // the computer's guess is can be the next guess
+            int i = 0;
+            // index - The index of the current guess
             int index = 0;
+            // This do while use to search number to computer's guess 
             do
             {
                 bool isFoundCombin = false;
-                i = 0;
-                for (; !isFoundCombin && i < allCombination.Length; i++)
+                
+                // This for loop stops if he finds a suitable number to guess
+                // and the next time he guesses he starts from the same place he stopped (from i)
+                for (; i < allCombination.Length; i++)
                 {
                     if (IsMatchCombination(allCombination[i]))
                     {
                         allGuess[index] = allCombination[i];
                         isFoundCombin = true;
+                        break;
                     }
                 }
 
@@ -34,16 +45,19 @@ namespace AiGuseeConsole
                     Console.ReadKey();
                     Environment.Exit(0);
                 }
-                Console.WriteLine("My " +( index + 1)  +" guess is: " + allCombination[i - 1]);
+
+                Console.WriteLine("My " +( index + 1)  +" guess is: " + allGuess[index]);
 
                 Console.WriteLine("Correct location:(black)");
                 int b = int.Parse(Console.ReadLine());
                 Console.WriteLine("Right number in wrong location:(white)");
                 int w = int.Parse(Console.ReadLine());
-
+                
 
                 responseToGuess[index] = new Tuple<int, int>(b, w);
             }
+            // Item1 is the correct location (black)
+            // and if is not 4 (ie the computer not managed to guess) then continue to search
             while (responseToGuess[index++].Item1 != 4);
 
             Console.WriteLine("I managed to guess at " + (index) + " guesses");
@@ -54,6 +68,9 @@ namespace AiGuseeConsole
         }
          static Tuple<int, int> compareTwoGuesses(int num1, int num2)
         {
+            // Gets 2 numbers and returns a pair representing the number of correct positions 
+            // between them (black) and those in the wrong places (white)
+            
             int correctLocationB = 0, notCorrectLocationW = 0;
             int[] arr1 = Int_to_array(num1);
             int[] arr2 = Int_to_array(num2);
@@ -62,6 +79,8 @@ namespace AiGuseeConsole
             {
                 if (arr1[i] == arr2[i])
                     correctLocationB++;
+                // Array.Exists - Returns true if array contains one or more elements that match the 
+                // conditions defined by the specified predicate; otherwise, false.
                 else if (Array.Exists(arr2, element => element == arr1[i]))
                     notCorrectLocationW++;
                 }
@@ -83,6 +102,8 @@ namespace AiGuseeConsole
         }
 
         static int[] getAllCombination() {
+            //The function returns an array of all 4-digit 
+            //numbers that include the digits between 1 and 6 without repeats
             List<int> tempList = new List<int>();
             for (int i = 1234; i <= 6543; i++)
                 if (existInGame(i))
@@ -93,6 +114,10 @@ namespace AiGuseeConsole
 
         private static bool existInGame(int num)
         {
+            //The function receives a 4-digit number
+            //and returns whether it is in the game, 
+            //ie whether it is between the numbers 1 to 6 without repetitions.
+
             ArrayList listOfDigitExist = new ArrayList() {1,2,3,4,5,6};
 
             while (num > 0)
@@ -106,7 +131,8 @@ namespace AiGuseeConsole
 
         static bool IsMatchCombination(int currentComb)
         {
-
+            // Gets a number and returns true whether it is possible to be the next guess. 
+            // Check by comparison with all previous guesses
             for (int i = 0; allGuess[i] != 0 && i < allGuess.Length; i++)
             {
                 if (!compareTwoGuesses(allGuess[i], currentComb).Equals(responseToGuess[i]))
